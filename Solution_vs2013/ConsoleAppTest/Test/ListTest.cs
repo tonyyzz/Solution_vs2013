1bad3e00eb7429edc02f4b4c6d1ed4a69c40da77
yzz <1277745281@qq.com>
2017-11-16 22:06:03 +0800

    -

--- Solution_vs2013/ConsoleAppTest/Test/ListTest.cs
+++ Solution_vs2013/ConsoleAppTest/Test/ListTest.cs
@@ -10,7 +10,23 @@ namespace ConsoleAppTest.Test
     {
         public void Do()
         {
-            var list = new List<int>() { 1, 2, 3, 556, 6 };
+            var list = new List<int>() { 1, 1, 2, 3, 556, 6 };
+            list = list.Distinct().ToList();
+            var list2 = list.ConvertAll(m =>
+            {
+                return m.ToString();
+            });
+            var f = list.ToDictionary(
+                (m) =>
+                {
+                    return m;
+                },
+                (n) =>
+                {
+                    return n.ToString();
+                });
         }
     }
+
+
 }
